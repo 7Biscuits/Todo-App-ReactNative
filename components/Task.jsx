95401a12bd8a7be1240b2e5afda403c7869b2cab
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-09-13 02:03:59 +0530

    refactor: prettier format

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,50 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+import { useState } from "react";
 
 const Task = ({ text }) => {
+  const [taskCompleted, setTaskCompleted] = useState(false);
+
   return (
-    <View style={styles.item}>
-      <View style={styles.itemLeft}>
-        <View style={styles.square}></View>
-        <Text style={styles.itemText}>{text}</Text>
-      </View>
-      <View style={styles.circular}></View>
+    <View>
+      <TouchableOpacity key={index} onPress={() => setTaskCompleted(!taskCompleted)}>
+        <View style={styles.item}>
+          <View style={styles.itemLeft}>
+            <View style={styles.square}></View>
+            <Text style={styles.itemText}>{text}</Text>
+          </View>
+          <View style={taskCompleted ? styles.circularFilled : styles.circularEmpty}></View>
+        </View>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   item: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     padding: 15,
     borderRadius: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
   },
   itemLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap'
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
   },
   square: {
     width: 24,
     height: 24,
-    backgroundColor: '#55BCF6',
+    backgroundColor: "#55BCF6",
     opacity: 0.4,
     borderRadius: 5,
     marginRight: 15,
   },
   itemText: {
-    maxWidth: '80%',
+    maxWidth: "80%",
   },
-  circular: {
+  circularEmpty: {
     width: 12,
     height: 12,
-    borderColor: '#55BCF6',
+    borderColor: "#55BCF6",
     borderWidth: 2,
     borderRadius: 5,
   },
+  circularFilled: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    backgroundColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  }
 });
 
 export default Task;
