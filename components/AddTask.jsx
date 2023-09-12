b420276029f22517af2c47b43ec93ac356943986
Rudransh Srivastava <rudranshsrivastava2525@gmail.com>
2023-09-13 02:04:30 +0530

    feat: write and add task

---
+++ components/AddTask.jsx
@@ -0,0 +1,74 @@
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+
+const AddTask = (props) => {
+  const [task, setTask] = useState();
+
+  const addTask = (task) => {
+    if (task == null) return;
+    props.newTask(task);
+  }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper}
+    >
+      <TextInput
+        style={styles.input}
+        placeholder={"Write a task"}
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <TouchableOpacity onPress={() => {
+        Keyboard.dismiss();
+        addTask(task);
+        setTask(null);
+      }}>
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+});
+
+export default AddTask;
